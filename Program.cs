d98d488b6f277c2d67440abdf05f73879abb0b1e
iSh0ck <lucasoeslick5@gmail.com>
2022-03-06 16:20:50 +0100

    Some functions added

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Compression;
+using System.IO;
 
 namespace Vela31_Ineo
 {
@@ -17,6 +19,57 @@ namespace Vela31_Ineo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Home());
+        }
+
+        /*
+         * 
+         * --------------------- A FAIRE ---------------------
+         * 
+         */
+        public static void DownloadDriver(String os, String model_name)
+        {
+            // Téléchargement du driver depuis internet
+        }
+
+        /*
+         * 
+         * ---------------------- A TEST ----------------------
+         * 
+         */
+        public static void UnzipArchive(String file)
+        {
+            // Unzip du fichier 
+            ZipFile.ExtractToDirectory(file, Directory.GetCurrentDirectory());
+        }
+
+        /*
+         * 
+         * --------------------- A FAIRE ---------------------
+         * 
+         */
+        public static void InstallDriver(String model_name, String ipaddr, String default_mode, String default_print)
+        {
+            // Installation du driver
+        }
+
+        /*
+         * 
+         * --------------------- A FAIRE ---------------------
+         * 
+         */
+        public static void SetupPrinterAsAdmin(String ipaddr)
+        {
+            // Paramétrage depuis l'interface web
+        }
+
+        /*
+         * 
+         * --------------------- A FAIRE ---------------------
+         * 
+         */
+        public static void SetupSMB(String agence)
+        {
+
         }
     }
 }
