f0caf7a2409af21f1501dc48233f053067e90372
iSh0ck <lucasoeslick5@gmail.com>
2022-06-27 19:12:39 +0200

    Stuff update

--- Scrapping.cs
+++ Scrapping.cs
@@ -1,5 +1,7 @@
-﻿using OpenQA.Selenium.Chrome;
-using System.Windows.Forms;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager.Helpers;
@@ -16,15 +18,35 @@ namespace Vela31_Ineo
 
             // Paramétrage des options de Chrome
             ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.AddArgument("--no-sandbox");
-            chromeOptions.AddArgument("--headless");
-            chromeOptions.AddArgument("--disable-gpu");
+            //chromeOptions.AddArgument("--no-sandbox");
+            //chromeOptions.AddArgument("--headless");
+            //chromeOptions.AddArgument("--disable-gpu");
 
             // Création du navigateur
             ChromeDriver chrome = new ChromeDriver(chromeOptions);
 
-            chrome.Navigate().GoToUrl("https://www.google.com/");
-            MessageBox.Show(chrome.Title);
+            chrome.Url = "http://192.168.40.241/wcd/system_device.xml";
+
+            //if (chrome.Url == "http://192.168.40.241/wcd/system_device.xml")
+            //{
+            //chrome.FindElement(By.Id("ScanFunction")).Click(); // Ouvrir le carnet d'adresses
+            //chrome.FindElement(By.Id("Regist")).Click(); // Nouvel enregistrement
+            //chrome.FindElement(By.Id("R_SEL3")).Click(); // Sélection du choix SMB
+            //chrome.FindElement(By.Id("Next")).Click(); // Validation du choix SMB
+            //chrome.FindElement(By.Id("T_NAM")).SendKeys("[Nom du destinataire]"); // Saisie du nom
+            //chrome.FindElement(By.Id("C_A_SMB_WEL")).Click(); // Ajout du favoris dans le carnet d'adresses
+            //chrome.FindElement(By.Id("C_A_SMB_C_HOS")).Click(); // Autoriser un nom d'hôte
+
+            // Penser à récupérer le nom d'hôte du PC
+            //chrome.FindElement(By.Id("T_HOS")).SendKeys("[Nom d'hôte]"); // Saisie du nom d'hôte
+            //chrome.FindElement(By.Id("C_A_SMB_DIR")).SendKeys("Scans"); // Indication du chemin du dossier
+            //chrome.FindElement(By.Id("T_LOG")).SendKeys("scan"); // Indication du nom d'utilisateur
+            // Penser à récupérer le mot de passe SMB
+            //chrome.FindElement(By.Id("P_PAS")).SendKeys("[Mot de passe]"); // Indication du mot de passe
+
+            // chrome.FindElement(By.Id("C_A_SMB_Apply")).Click(); // Ajout du destinataire
+            //}
+
             chrome.Quit();
         }
     }
