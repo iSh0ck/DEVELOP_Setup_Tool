7d63e327be15f6d2efe2e00aadef4038f1825ca4
/-_Sh0ck <30291488+iSh0ck@users.noreply.github.com>
2023-06-16 15:24:55 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -46,7 +46,7 @@ namespace Vela31_Ineo
         {
             // Téléchargement du driver depuis internet
             // Adresse du serveur de téléchargement
-            String server = "shkhome.ddns.net:8080";
+            String server = "drivers.shkdev.fr:8080";
 
             // Création du dossier téléchargement
             if (!Directory.Exists(Directory.GetCurrentDirectory() + @"\Download"))
